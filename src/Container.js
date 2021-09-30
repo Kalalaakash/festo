44d14f1a1b8139edee8dd9e8882ed0e24ff4be97
Kalalaakash <72199686+Kalalaakash@users.noreply.github.com>
2021-09-30 21:21:03 +0530

    Update Container.js

--- src/Container.js
+++ src/Container.js
@@ -12,11 +12,11 @@ function Container() {
             <p>Makar Sankranti is a Hindu harvest festival which 
                 falls on a fixed Gregorian date of January 14th every 
                 year with an exception of a few years when the festival 
-                is celebrated on January 15th; <b> Makar Sankranti 
-                2021 will be celebrated on 14th January 2021. </b> </p>
+                is celebrated on January 15-th; <b> Makar Sankranti 
+                2021 will be celebrated on 14-th January 2021. </b> </p>
             </div>
             <div className="info">
-                <h2>Know more about Sankranti</h2>
+                <h2>Know more about Sankranti!!</h2>
             </div>
         </section>
     );
